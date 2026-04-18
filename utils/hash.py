9c6e6b2f52ccfdacfac0b77e2fe63762db649d01
EvieFoxes <wolfyasgreywolf@gmail.com>
2026-04-18 16:05:59 +0200

    0.0.2

--- utils/hash.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import hmac
-
-
-def hmac_sha1(app_token, challenge):
-    return hmac.new(
-        app_token.encode("utf-8"), challenge.encode("utf-8"), hashlib.sha1
-    ).hexdigest()
